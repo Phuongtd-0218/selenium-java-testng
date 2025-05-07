7ab9e82742beb24a033063cfce6a45f6dc3c5ed4
PhuongTD02 <phuong95haui@gmail.com>
2025-05-07 23:51:54 +0700

    edit

---
+++ selenium/webdriver/part3/Topic_10_Textbox_Textarea.java
@@ -0,0 +1,46 @@
+package webdriver.part3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_10_Textbox_Textarea {
+
+    //JUnit - dùng cho Unit Test
+    //TestNG - dùng cho UI test
+
+    // Thành phần/Cấu trúc trong 1 test case
+
+    // 1- Setup: Os/Browser/Web/Page/Data/Variable/Object/...
+    WebDriver driver;
+
+    @BeforeClass
+    public void initialBrowser(){
+        driver = new FirefoxDriver();
+
+        driver.get("https://demo.nopcommerce.com");
+    }
+
+    // 2- Action/Execute: Tương tác lên element nào/nhập liệu/verify..
+    // Cần có chỉ dẫn - annotations để testcase hoạt động
+    @Test
+    public void TC_01_(){
+
+
+    }
+
+    @Test
+    public void TC_02_(){
+
+
+    }
+
+    // 3- Clean: Delete data test/account/close browser/...
+    @AfterClass
+    public void cleanBrowser(){
+        driver.quit();
+    }
+
+}
